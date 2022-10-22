5394c3c3365d6e336566d6e375c6cbb8c6df777e
Abui <adjiem31@gmail.com>
2022-10-22 18:40:55 +0700

    test

--- index.js
+++ index.js
@@ -14,14 +14,40 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/fee-assessment-books', async (req, res) => {
+  // replace with a custom URL as required
+
+  try {
+    console.log(req.query);
+    const response = await axios.get(
+      'https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-books',
+      { params: req.query }
+    );
+
+    res.send(response.data);
+  } catch (e) {
+    if (e.response.status === 404) {
+      res.status(200).send([]);
+    } else {
+      res.status(e.response.status).send(e);
+    }
+  }
+});
+
 // the route we're working with
 app.get('/fee-assessment-categories', async (req, res) => {
   // replace with a custom URL as required
-  const response = await axios.get(
-    'https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories',
-    req
-  );
-  res.send(response.data);
+  try {
+    const response = await axios.get(
+      'https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories',
+      {
+        params: req.query.params,
+      }
+    );
+    res.send(response.data);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 // console text when app is running
